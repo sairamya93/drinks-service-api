5878efb8b1092fd9331e15c523a8942512774d31
Ramya <123948635+sairamya93@users.noreply.github.com>
2024-03-08 15:53:51 +0000

    feat: Implemented JSON response for coffee controller and tested

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -7,4 +7,12 @@ const res = await request(app).get('/coffeelover');
 expect(res.statusCode).toEqual(200);
 expect(res.text).toEqual('I like coffee!');
 });
+
+test('GET /coffee should return correct object', async () => {
+    const res = await request(app)
+    .get('/coffee')
+    .query({ coffeeName: 'Latte' });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({drinkType: 'Coffee',name: 'Latte',});
+    });
 });

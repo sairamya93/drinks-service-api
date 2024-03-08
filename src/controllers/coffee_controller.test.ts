95733c9fa361b1f107293002e47967d05f29db50
Ramya <123948635+sairamya93@users.noreply.github.com>
2024-03-08 23:26:13 +0000

    Execute on server with the command npx tsx src/server.ts

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,11 +8,26 @@ expect(res.statusCode).toEqual(200);
 expect(res.text).toEqual('I like coffee!');
 });
 
-test('GET /coffee should return correct object', async () => {
+test('GET /coffee should return correct object with name Latte', async () => {
     const res = await request(app)
     .get('/coffee')
     .query({ coffeeName: 'Latte' });
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({drinkType: 'Coffee',name: 'Latte',});
     });
+
+test('GET /coffee should return correct object with name Cappucinno', async () => {
+        const res = await request(app)
+        .get('/coffee')
+        .query({ coffeeName: 'Cappucinno' });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({drinkType: 'Coffee',name: 'Cappucinno',});
+        });
+
+test('GET /coffee should return default coffee name if no parameter is specified', async () => {
+            const res = await request(app)
+            .get('/coffee')
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toEqual({drinkType: 'Coffee',name: 'Latte',});
+            });
 });
